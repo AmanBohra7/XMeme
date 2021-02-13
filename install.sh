afc55e914e5e749ebc5d3dc2770e208136817b93
AmanBohra7 <52762311+AmanBohra7@users.noreply.github.com>
2021-02-14 04:49:47 +0530

    updating install.sh file

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 
 cd Server/
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+. ~/.nvm/nvm.sh
+
+sudo nvm install -y node
+
 curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
